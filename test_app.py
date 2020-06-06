1825b7e7be2ef30a889a72470f1ee86580e4cd4b
edesz <elsdes3@gmail.com>
2020-06-06 16:06:02 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,10 +7,6 @@ from app import show_msg
 from kubernetes import client, config
 
 
-def test_show_msg():
-    assert show_msg() == "hello"
-
-
 def test_abc():
     config.load_kube_config()
     v1 = client.CoreV1Api()
@@ -23,23 +19,23 @@ def test_abc():
         )
 
 
-# @pytest.mark.parametrize("num_pods", (3,))
-# @pytest.mark.repeat(1)
-# @pytest.mark.applymanifests("configs", files=["nginx.yaml"])
-# def test_nginx(kube, kubeconfig, num_pods):
-#     """An example test against an Nginx deployment."""
+@pytest.mark.parametrize("num_pods", (3,))
+@pytest.mark.repeat(10)
+@pytest.mark.applymanifests("configs", files=["nginx.yaml"])
+def test_nginx(kube, kubeconfig, num_pods):
+    """An example test against an Nginx deployment."""
 
-#     # wait for the manifests loaded by the 'applymanifests' marker
-#     # to be ready on the cluster
-#     kube.wait_for_registered(timeout=30)
+    # wait for the manifests loaded by the 'applymanifests' marker
+    # to be ready on the cluster
+    kube.wait_for_registered(timeout=30)
 
-#     deployments = kube.get_deployments()
-#     nginx_deploy = deployments.get("nginx-deployment")
-#     assert nginx_deploy is not None
+    deployments = kube.get_deployments()
+    nginx_deploy = deployments.get("nginx-deployment")
+    assert nginx_deploy is not None
 
-#     pods = nginx_deploy.get_pods()
-#     assert len(pods) == num_pods, f"nginx expected with {num_pods} replicas"
+    pods = nginx_deploy.get_pods()
+    assert len(pods) == num_pods, f"nginx expected with {num_pods} replicas"
 
-#     for pod in pods:
-#         containers = pod.get_containers()
-#         assert len(containers) == 1, "nginx pod should have one container"
+    for pod in pods:
+        containers = pod.get_containers()
+        assert len(containers) == 1, "nginx pod should have one container"
