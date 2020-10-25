0de7e76548a0126c02c8e163ab12a99c576d5049
edesz <elsdes3@gmail.com>
2020-10-25 15:07:04 -0400

    Update gcp_startup.sh

--- gcp_startup.sh
+++ gcp_startup.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
 
-pip install -r requirements.txt
+ACTION=${1:-create}
+
+
+if [[ $ACTION == "clone" ]]
+then
+    # (OPTION 1) clone a project containing requirements.txt
+    git clone <project_url>
+if [[ $ACTION == "create" ]]
+then
+    # (OPTION 2) create requirements.txt
+    cat > requirements.txt << EOF
+    nb_black
+    nodeenv
+    jupyter_contrib_nbextensions
+    jupyter_nbextensions_configurator
+    numpy
+    pandas
+    scikit-learn
+    matplotlib
+    seaborn
+    imbalanced-learn
+    yellowbrick
+    azure-storage-blob
+    xgboost
+    xlrd
+    category_encoders
+    EOF
+fi
+
+# Install Python packages
+pip install -U -r requirements.txt
